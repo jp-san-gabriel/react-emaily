4783583e56eba4527ab4cf321373846a6ed0b943
jp <jp.san.gabriel01@gmail.com>
2018-08-05 15:24:27 +0800

    Refactor: move passport-related codes to separate file

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport.js');
 
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
-    }
-  )
-);
 
 app.get(
   '/auth/google',
