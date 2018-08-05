1de147dc74819955ee44bf07b7babcd058d88d8d
jp <jp.san.gabriel01@gmail.com>
2018-08-05 13:41:33 +0800

    Get client id and secret from console.developers.google.com

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+// clientID 638362341365-an7jd9mjl8bqho256g9a9fao6ikg86uo.apps.googleusercontent.com
+// clientSecret r274BkHUdlEfBWgmagJER44f
 passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
