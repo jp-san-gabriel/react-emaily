db5cd2807cd9043413516439abe74571bb11a9cc
jp <jp.san.gabriel01@gmail.com>
2018-08-13 00:47:19 +0800

    Render 'Payments' component on 'Header'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -12,7 +13,10 @@ class Header extends Component {
           <li><a href="/auth/google">Login With Google</a></li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return [
+          <li><Payments /></li>,
+          <li><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
@@ -21,7 +25,7 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link to={ this.props.auth ? '/surveys' : '/' } 
+          <Link to={ this.props.auth ? '/surveys' : '/' }
             className="left brand-logo"
           >
             Emaily
