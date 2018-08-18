8600c3405353ea85f37db1d7b2d23e1cfd2173a7
jp <jp.san.gabriel01@gmail.com>
2018-08-18 12:11:05 +0800

    Call addContent in Mailer constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,6 +10,8 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
   }
 
   formatAddresses(recipients) {
