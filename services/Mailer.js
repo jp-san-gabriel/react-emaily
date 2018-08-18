1f092f4e181879822c18e342ab9a452d4d45f0bc
jp <jp.san.gabriel01@gmail.com>
2018-08-18 12:13:08 +0800

    Add click tracking to Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -12,12 +12,21 @@ class Mailer extends helper.Mail {
     this.recipients = this.formatAddresses(recipients);
 
     this.addContent(this.body);
+    this.addClickTracking();
   }
 
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
       return new helper.Email(email);
     });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
   }
 }
 
