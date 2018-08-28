b893fb49a69d9d7a182cee48eefe18a3fd78b068
jp <jp.san.gabriel01@gmail.com>
2018-08-28 15:52:35 +0800

    Send the validation error message to the form fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,13 +2,14 @@
 // label and text input
 import React from 'react';
 
-export default({ input, label }) => {
+export default({ input, label, meta }) => {
   return (
     <div>
       <label>{label}</label>
       {/* the next line is equivalent to...
       <input onBlur={input.onBlur} onChange={input.onChange} /> */}
       <input { ...input }/>
+      {meta.error}
     </div>
   );
 }
