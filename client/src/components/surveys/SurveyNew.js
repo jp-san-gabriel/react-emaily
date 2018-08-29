43b5b734fdb53e08ca9b920559d16c566c03fecf
jp <jp.san.gabriel01@gmail.com>
2018-08-29 11:29:24 +0800

    Render SurveyFormReview component conditionally based on the value of state.showFormReview

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,24 @@
 //SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
 
   state = { showFormReview: false };
 
+  renderContent() {
+    if(this.state.showFormReview === true) {
+      return <SurveyFormReview />;
+    }
+
+    return <SurveyForm />;
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.renderContent()}
       </div>
     );
   }
