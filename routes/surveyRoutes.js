dac53dcffe126d487e9c83695a100c02db3b9b6f
jp <jp.san.gabriel01@gmail.com>
2018-08-30 23:53:52 +0800

    Refactor: Use the extracted email and url from the destructured event object

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,13 +17,14 @@ module.exports = app => {
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(event.url).pathname;
+      const pathname = new URL(url).pathname;
       const p = new Path('/api/surveys/:surveyId/:choice');
       const match = p.test(pathname);
       if(match) {
-        return { email: event.email, surveyId: match.surveyId, choice: match.choice };
+        return { email, surveyId: match.surveyId, choice: match.choice };
       }
     });
+    console.log(events);
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
